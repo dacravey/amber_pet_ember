5d9cef0549978a59c26c369b60e8cbc5b7f954b1
Guest User <student@epicodus.com>
2015-04-07 13:27:44 -0700

    add initial files

---
+++ ember/tests/integration/landing-page-test.js
@@ -0,0 +1,76 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+import Pretender from 'pretender';
+
+var App, server;
+
+module('Integration - Landing Page', {
+  beforeEach: function() {
+    App = startApp();
+
+    var pets = [
+    {
+    id: 1,
+    name: 'Bugs Bunny',
+    description: 'fluffy',
+    age: '5',
+    breed: 'Collie',
+    user_id: '2',
+    chip_number: '23431'
+    },
+    {
+    id: 2,
+    name: 'Fido',
+    description: 'snarly',
+    age: '5',
+    breed: 'poodle',
+    user_id: '3',
+    chip_number: '85945'
+    },
+    {
+    id: 3,
+    name: 'Joel',
+    description: 'shy',
+    age: '1',
+    breed: 'Maine Coone',
+    user_id: '3',
+    chip_number: '89742'
+    }
+    ];
+
+    server = new Pretender(function() {
+      this.get('/api/pets', function(request) {
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({pets: pets})];
+      });
+
+      this.get('/api/pets/:id', function(request) {
+        var pet = pets.find(function(pet) {
+          if (pet.id === parseInt(request.params.id, 10)) {
+            return pet;
+          }
+        });
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({pet: pet})];
+      });
+    });
+  },
+
+  afterEach: function() {
+    Ember.run(App, 'destroy');
+    server.shutdown();
+  }
+});
+
+
+
+test('Should welcome me to Amber Pet', function(assert) {
+  visit('/').then(function() {
+    assert.equal(find('h2#title').text(), 'Welcome to Amber Pet');
+  });
+});
+
+test('Should list all the pets m', function(assert) {
+  visit('/').then(function() {
+    assert.equal(find('h2#title').text(), 'Welcome to Amber Pet');
+  });
+});
