9437caf08a7f242c81fdd5592c6d1525e2c17752
Debbie Cravey <cinemaestro@yahoo.com>
2015-04-09 10:10:58 -0700

    add API controllers for losts and findings and add user and pet id to pet controller

--- rails/app/controllers/api/losts_controller.rb
+++ rails/app/controllers/api/losts_controller.rb
@@ -6,6 +6,37 @@ class Api::LostsController < ApplicationController
     render json: @losts
   end
 
+  def show
+    render json: Lost.find(params[:id])
+  end
 
+  def update
+    @lost = Lost.find(params[:id])
+    if @lost.update(lost_params)
+      render json: @lost
+    else
+      render json: @lost.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @lost = Lost.new(lost_params)
+    if @lost.save
+      render json: @lost, status: :created, location: @lost
+    else
+      render json: @lost.errors, status: :unprocessable_eintity
+    end
+  end
+
+  def destroy
+    @lost = Lost.find(params[:id])
+    @lost.destroy
+    head :no_content
+  end
+
+  private
+  def lost_params
+    params.require(:lost).permit(:date, :address, :time, :pet_id)
+  end
 
 end
