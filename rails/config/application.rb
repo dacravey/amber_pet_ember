e394fc5410f8658f5e07e862d8531d7db34c58f3
Debbie Cravey <cinemaestro@yahoo.com>
2015-04-09 23:46:05 -0700

    change configs for Heroku production environment

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -30,6 +30,7 @@ module AmberPet
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.ember.variant = :production
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
