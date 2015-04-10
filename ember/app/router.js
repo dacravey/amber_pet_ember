3fe317401c88c9b5f6f46af6a11112379e87d15d
Dex Fitch <fitch.dexter@gmail.com>
2015-04-10 11:59:23 -0700

    Fix refresh error

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,17 +7,17 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
   this.resource('pets', function() {
-    this.route('index', {path: '../pets'});
+    this.route('index', {path: '/'});
     this.route('show', {path: ':pet_id'});
     this.route('new', {path: 'new'});
   });
   this.resource('losts', function() {
-    this.route('index');
+    this.route('index', {path: '/'});
     this.route('show', {path: ':lost_id'});
     this.route('new', {path: 'new'});
   });
   this.resource('findings', function() {
-    this.route('index');
+    this.route('index', {path: '/'});
     this.route('show', {path: ':finding_id'});
     this.route('new', {path: 'new'});
   });
